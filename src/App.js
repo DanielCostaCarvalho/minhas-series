56faa5e5280b4eeffa19af93f3e43cc85691f6db
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-08-06 15:54:03 -0300

    Recebendo informações dos gêneros via axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import Header from './Header';
+import React, { useEffect, useState } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import axios from 'axios'
+import Header from './Header';
 import Generos from './Generos';
 import Home from './Home'
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get('/api').then(res => {
+      setData(res.data)
+    })
+  }, [])
   return (
     <Router>
     <div className="App">
       <Header/>
       <Route path='/' exact component={Home}/>
       <Route path='/generos' component={Generos}/>
+      <pre>{JSON.stringify(data)}</pre>
     </div>
     </Router>
   );
--- src/Generos.js
+++ src/Generos.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button, Alert } from 'reactstrap';
+import axios from 'axios';
+
+const renderizaLinha = row => {
+    return (
+    <tr key={row.id}>
+        <th scope='row'>{row.id}</th>
+        <td>{row.name}</td>
+        <td><Button outline color='secondary'>Adicionar</Button></td>
+    </tr>
+    )
+}
 
 const Generos = () => {
-    return <h1>Gêneros</h1>;
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('/api/genres')
+            .then(res => {
+                setData(res.data.data);
+            })
+    }, [])
+
+    if(data.length === 0){
+        return(
+            <div className='container'>
+                <h1>Gêneros</h1>
+                <Alert color="dark">
+                    Você não possui gêneros criados
+                </Alert>
+            </div>
+        )
+    }
+
+    return (
+    <div className='container'>
+    <h1>Gêneros</h1>
+    <table className='table table-striped' >
+        <thead>
+            <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Nome</th>
+                <th scope='col'>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            {data.map(renderizaLinha)}
+        </tbody>
+    </table>
+    </div>
+    );
 };
 
 export default Generos;
